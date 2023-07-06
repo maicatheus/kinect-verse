6c561426e7cfdfc2c54025ff4fc8bdee153cf22b
matheus_maica <maicatheus@gmail.com>
2023-07-06 01:05:20 -0300

    Controle total do carro

--- app_volante.py
+++ app_volante.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import keyboard
 import time
 import math
 
@@ -7,22 +8,34 @@ def calculate_angle(p1,p2):
     return math.tan(((p2[1]-p1[1])/(p2[0]-p1[0])))
 
 def control_car(angle):
-    if(angle < -0.15):
+    if(angle > 0.15):
+        keyboard.release('right')
+        keyboard.press("left")
         print("Direita")
-    elif(angle > 0.15):
+    elif(angle < -0.15):
+        keyboard.release('left')
+        keyboard.press("right")
         print("Esquerda")
     else:
+        keyboard.release('right')
+        keyboard.release('left')
         print("Reto")
-
-def distancia_entre_pontos(p1,p2):
-    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 def drive_car(p1,p2):
     if(p1[4][1] < p1[3][1] and p2[4][1] < p2[3][1]):
-        print("ACELERA!")
-    
-    if(distancia_entre_pontos(p1[0],p2[0]) > 300):
+        keyboard.release('s')
+        keyboard.press("w")
+        print("ACELERA!")   
+    elif((p1[4][1] > p1[3][1] and p2[4][1] < p2[3][1]) or (p1[4][1] < p1[3][1] and p2[4][1] > p2[3][1])):
+        keyboard.release('w')
+        keyboard.press("s")
         print("FREIA!")
+    else:
+        keyboard.release('w')
+        keyboard.release('s')
+
+def distancia_entre_pontos(p1,p2):
+    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 camera = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
