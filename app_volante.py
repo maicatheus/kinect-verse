55a92e88f034c6b29c7f322d7871eb00c36e7400
matheus_maica <maicatheus@gmail.com>
2023-07-07 23:57:47 -0300

    alteraçoes

--- app_volante.py
+++ app_volante.py
@@ -1,87 +1,84 @@
 import cv2
-import mediapipe as mp
-import keyboard
-import time
 import math
+import vgamepad as vg
+import mediapipe as mp
 
-def calculate_angle(p1,p2):
-    return math.tan(((p2[1]-p1[1])/(p2[0]-p1[0])))
 
-def control_car(angle):
-    if(angle > 0.15):
-        keyboard.release('right')
-        keyboard.press("left")
+def normalixar_angulo(angulo,a_min,a_max):
+    return((abs(angulo)- a_min)/(a_max-a_min))
+
+def calcular_angulo(p1,p2):
+    theta = math.atan((p2[1] - p1[1])/(p2[0] - p1[0]))
+    return theta*180/math.pi
+
+def controlar_velocidade(pontos):
+    hand1,hand2 = pontos
+    if(hand1[4][1]<hand1[3][1] and hand2[4][1]<hand2[3][1]):
+        gamepad.left_trigger_float(0)
+        gamepad.right_trigger_float(0.86)
+        print("Acelera!")
+    elif(hand1[4][1]<hand1[3][1] and hand2[4][1]>hand2[3][1] or hand1[4][1]>hand1[3][1] and hand2[4][1]<hand2[3][1]):
+        gamepad.right_trigger_float(0)
+        gamepad.left_trigger_float(1)
+        print("Freio!")
+    else:
+        gamepad.right_trigger_float(0)
+        gamepad.left_trigger_float(0)
+
+def controlar_volante(angulo):
+    a_min, a_max = 6, 45
+    
+    if angulo > a_min:
+        if angulo > a_max:
+            angulo = a_max
+        gamepad.left_joystick_float(-normalixar_angulo(angulo,a_min,a_max),0)
         print("Esquerda")
-    elif(angle < -0.15):
-        keyboard.release('left')
-        keyboard.press("right")
+
+    elif angulo < -a_min:
+        if angulo < - a_max:
+            angulo = -a_max
+        gamepad.left_joystick_float(normalixar_angulo(angulo,a_min,a_max),0)
         print("Direita")
+    
     else:
-        keyboard.release('right')
-        keyboard.release('left')
+        gamepad.left_joystick_float(0,0)
         print("Reto")
-
-def drive_car(p1,p2):
-    if(p1[4][1] < p1[3][1] and p2[4][1] < p2[3][1]):
-        keyboard.release('s')
-        keyboard.press("w")
-        print("ACELERA!")   
-    elif((p1[4][1] > p1[3][1] and p2[4][1] < p2[3][1]) or (p1[4][1] < p1[3][1] and p2[4][1] > p2[3][1])):
-        keyboard.release('w')
-        keyboard.press("s")
-        print("FREIA!")
-    else:
-        keyboard.release('w')
-        keyboard.release('s')
-
-def distancia_entre_pontos(p1,p2):
-    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
+    
 
 camera = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_PLAIN
-scale = 3
-pTime = 0
-color = (255,0,0)
-thickness = 3
 
 mpDraw = mp.solutions.drawing_utils
-mpHand = mp.solutions.hands
-hands = mpHand.Hands(max_num_hands=2)
-
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=2)
+gamepad = vg.VX360Gamepad()
 while True:
-    try:
-        ret, frame = camera.read()
-        frameRgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(frameRgb)
-        double_hands = ([],[])
-        if results.multi_hand_landmarks:
-            for i,handsLms in enumerate(results.multi_hand_landmarks):
-                for id, lm in enumerate(handsLms.landmark):
-                    h, w, c = frame.shape
-                    cx, cy= int(lm.x*w), int(lm.y*h)
-                    double_hands[i].append((cx,cy))
+    ret, frame = camera.read()
 
-                mpDraw.draw_landmarks(frame, handsLms, mpHand.HAND_CONNECTIONS)
-                try:
-                    angle = calculate_angle(double_hands[0][0],double_hands[1][0])
-                    control_car(angle)
-                    drive_car(double_hands[0], double_hands[1])
-                    cv2.line(frame, double_hands[0][0], double_hands[1][0], color, 1)
-                except:
-                    pass
+    frameRgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    results = hands.process(frameRgb)
 
-        cTime = time.time()
-        fps = 1/(cTime - pTime)
-        pTime = cTime
-        # cv2.putText(frame,str(f"angulo: {int(angle)}"),(0,50),font,scale,color,thickness)
-        cv2.putText(frame,str(f"FPS: {int(fps)}"),(0,40),font,scale,color,thickness)
+    double_hands = ([],[])
+    if results.multi_hand_landmarks:
+        for index, handsLms in enumerate(results.multi_hand_landmarks):
+            for id, lm in enumerate(handsLms.landmark):
+                h, w, _ = frame.shape
+                cx , cy = int(lm.x*w), int(lm.y*h)
+                double_hands[index].append((cx,cy))
 
-        cv2.imshow("Kinect Verse", frame)
+            mpDraw.draw_landmarks(frame,handsLms,mpHands.HAND_CONNECTIONS)  
+        
+        try:
+            angulo = calcular_angulo(double_hands[0][0],double_hands[1][0])
+            controlar_volante(angulo)
+            controlar_velocidade(double_hands)
+            gamepad.update()
+            cv2.line(frame,double_hands[0][0],double_hands[1][0],(255,0,0),3)
+        except:
+            print("Problema")
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    except:
-        print("Algum problema!")
 
-camera.release()
-cv2.destroyAllWindows()
+    cv2.imshow("Volante virtual",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
